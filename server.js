7de2d168316489391aa58a95d6c1be35cbef88c1
Adeeba-Khan <adeebacs0008@gmail.com>
2026-06-20 17:44:15 +0530

    insert and get (4)

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const mysql = require('mysql2');
 // const db = require('./utils/db-connection');
 const router = require('./routes/studentRoutes');
+const router1 = require('./routes/userRoutes');
+const router2 = require('./routes/busRoutes');
 const app = express();
 app.use(express.json());
 
 app.use('/students',router);
+app.use('/user',router1);
+app.use('/bus',router2);
 app.listen(3000,(err)=>{
     if(err){console.log("server fails to run");
     }
